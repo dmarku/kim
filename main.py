7a381842487bd462394a141e211a720cbeeba5c2
Markus Dittmann <markus.dittmann@kodira.de>
2023-04-05 23:11:25 +0200

    Move config file parsing into a separate function

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 import argparse
 import requests
 import tomllib
+from dataclasses import dataclass
+
+@dataclass
+class Configuration:
+    """Runtime configuration options for 'kim'."""
+    api_url: str
+    user: str
+    api_key: str
 
 def require_string(dict_, name):
     assert(name in dict_ and type(dict_[name]) == str)
     return dict_[name]
 
+def read_config(filename):
+    with open(filename, 'rb') as f:
+        config = tomllib.load(f)
+
+        return Configuration(
+            api_url = require_string(config, 'api_url'),
+            user    = require_string(config, 'user'),
+            api_key = require_string(config, 'api_key'),
+        )
+    
 def run():
     parser = argparse.ArgumentParser(
       prog="kimai API client",
@@ -21,19 +39,13 @@ def run():
     )
 
     args = parser.parse_args()
+    config = read_config(args.config_file)
 
-    with open(args.config_file, 'rb') as f:
-      config = tomllib.load(f)
-
-      api_url = require_string(config, 'api_url')
-      api_key = require_string(config, 'api_key')
-      user    = require_string(config, 'user')
-
-    url = api_url + '/ping'
-    headers = {'x-auth-user': user, 'x-auth-token': api_key}
+    url = config.api_url + '/ping'
+    headers = {'x-auth-user': config.user, 'x-auth-token': config.api_key}
     response = requests.get(url, headers=headers)
     json = response.json()
     print(f'response body JSON is {json}')
 
 if __name__ == "__main__":
-  run()
+    run()
