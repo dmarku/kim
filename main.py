d9e6e54026acba6c413a18f5270310c621204ad5
Markus Dittmann <markus.dittmann@kodira.de>
2023-04-05 22:48:50 +0200

    Guard against accidental execution as a program

--- main.py
+++ main.py
@@ -3,28 +3,37 @@ import requests
 import tomllib
 
 def require_string(dict_, name):
-  assert(name in dict_ and type(dict_[name]) == str)
-  return dict_[name]
+    assert(name in dict_ and type(dict_[name]) == str)
+    return dict_[name]
 
-parser = argparse.ArgumentParser(
-  prog="kimai API client",
-  description="makes API calls to the Kimai API",
-)
+def run():
+    parser = argparse.ArgumentParser(
+      prog="kimai API client",
+      description="makes API calls to the Kimai API",
+    )
 
-parser.add_argument('--config', '-c', metavar='configuration file', dest='config_file', default='kim.toml')
+    parser.add_argument(
+        '--config',
+        '-c',
+        metavar='configuration file',
+        dest='config_file',
+        default='kim.toml',
+    )
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-with open(args.config_file, 'rb') as f:
-  config = tomllib.load(f)
+    with open(args.config_file, 'rb') as f:
+      config = tomllib.load(f)
 
-  api_url = require_string(config, 'api_url')
-  api_key = require_string(config, 'api_key')
-  user    = require_string(config, 'user')
+      api_url = require_string(config, 'api_url')
+      api_key = require_string(config, 'api_key')
+      user    = require_string(config, 'user')
 
-url = api_url + '/ping'
-print(headers)
-response = requests.get(url, headers=headers)
-json = response.json()
-print(f'response body JSON is {json}')
+    url = api_url + '/ping'
+    headers = {'x-auth-user': user, 'x-auth-token': api_key}
+    response = requests.get(url, headers=headers)
+    json = response.json()
+    print(f'response body JSON is {json}')
 
+if __name__ == "__main__":
+  run()
