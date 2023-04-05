d0142cee10ee4d2c5b40f839adf99b3784c1682f
Markus Dittmann <markus.dittmann@kodira.de>
2023-04-05 23:21:07 +0200

    Move 'ping' to a subcommand

--- main.py
+++ main.py
@@ -23,6 +23,13 @@ def read_config(filename):
             user    = require_string(config, 'user'),
             api_key = require_string(config, 'api_key'),
         )
+
+def ping(args, config):
+    url = config.api_url + '/ping'
+    headers = {'x-auth-user': config.user, 'x-auth-token': config.api_key}
+    response = requests.get(url, headers=headers)
+    json = response.json()
+    print(f'response body JSON is {json}')
     
 def run():
     parser = argparse.ArgumentParser(
@@ -38,14 +45,14 @@ def run():
         default='kim.toml',
     )
 
+    command_parsers = parser.add_subparsers(title='command', dest='command', metavar='command', required=True)
+
+    ping_parser = command_parsers.add_parser('ping', help='check API availability')
+    ping_parser.set_defaults(command_func=ping)
+
     args = parser.parse_args()
     config = read_config(args.config_file)
-
-    url = config.api_url + '/ping'
-    headers = {'x-auth-user': config.user, 'x-auth-token': config.api_key}
-    response = requests.get(url, headers=headers)
-    json = response.json()
-    print(f'response body JSON is {json}')
+    args.command_func(args, config)
 
 if __name__ == "__main__":
     run()
